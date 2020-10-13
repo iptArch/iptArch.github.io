068369097d6a4523e996d01252524b76b1352cde
Zakery Clarke <zakerytclarke@gmail.com>
2020-10-12 20:08:30 -0600

    Init

---
+++ index.js
@@ -0,0 +1,67 @@
+var PARAMS=getParams();
+
+
+var colors=["#23c1e3","#f58240","#ed028c","#c4d830","#fff101","#ff382d"];
+
+sd = new showdown.Converter(),
+renderPortfolio();
+
+
+
+async function renderPortfolio(){
+  fetch("https://api.github.com/repos/zakerytclarke/ipt/issues")
+    .then(x=>x.json())
+    .then(async function(data){
+      console.log(data);
+      out=[];
+      for(var i=0;i<data.length;i++){
+        if(PARAMS.id){
+          if(PARAMS.id==data[i].id){
+            var temp=await renderPortfolioItem(data[i],i);
+            out.push(temp);
+          }
+        }else{
+          var temp=await renderPortfolioItem(data[i],i);
+          out.push(temp);
+        }
+      }
+      html=out.reduce((acc,cur)=>acc+cur,"");
+      document.getElementById("portfolioBody").innerHTML=html;
+    })
+}
+
+
+async function renderPortfolioItem(data,i){
+  var num=i;
+  if(i<10){
+    num="0"+i;
+  }
+  console.log(data)
+  if(data.state!="open"){
+    return "";
+  }
+  html=sd.makeHtml(data.body);
+  let response = await fetch(data.comments_url).then(x=>x.json());
+  console.log(data.comments_url,response);
+  comments=response.map(x=>sd.makeHtml(x.body));
+  var commentsHTML=comments.reduce((acc,cur)=>acc+cur,"");
+  return `<a href="./project.html?id=${data.id}">
+            <div class="portfolioItem">
+              <h2>${data.title||""}</h2>
+              <span class="numberHeader" style="color:${colors[i%colors.length]}"><b><i>${num}</i></b></span>
+              ${html}
+              <div class="comments">${commentsHTML}</div>
+            </div>
+          </a>`;
+
+}
+
+
+function getParams(){
+  var data=window.location.search.substring(1).split("&").map(x=>x.split("="));
+  var out={};
+  data.map(function(x){
+    out[x[0]]=x[1];
+  })
+  return out;
+}
